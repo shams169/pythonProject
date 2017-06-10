fc41210382d8f612b51d1aa6c461e5a69d85bd3f
shams <shamssss.169@gmail.com>
2017-06-10 13:12:40 -0600

    initial commit

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+wsgi_app = app.wsgi_app
+
+# @app.route('/')
+# def hello():
+#     return "Hello World!"
+
+#import routes file
+#from ContactsDir.routes import *;
+from routes import *
 
 if __name__ == '__main__':
+
     app.run()
+
+
